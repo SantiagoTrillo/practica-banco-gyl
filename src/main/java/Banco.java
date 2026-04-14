785552550f37a5569d6da81f08caa45b6a6d5120
Santiago Trillo <santitrillo07@gmail.com>
2026-04-14 13:12:44 -0300

    Feature/144/Remoción del atributo nombre

--- src/main/java/Banco.java
+++ src/main/java/Banco.java
@@ -1,61 +1,34 @@
 import java.util.ArrayList;
 
 public final class Banco {
-    private String nombre;
     private static Banco instancia;
     private final ArrayList<Sucursal> sucursales;
 
-    private Banco(String nombre) throws IllegalArgumentException {
-        setNombre(nombre);
-        sucursales = new ArrayList<>();
+    private Banco() {
+        sucursales = new ArrayList<>(
+
+        );
     }
 
     public void crearSucursal(String nombre) {
         if (nombre.isBlank()) {
             throw new IllegalArgumentException("(Banco, crearSucursal) el nombre de la sucursal está vacío");
         }
-        Sucursal sucursalNueva = new Sucursal(nombre);
-        sucursales.add(sucursalNueva);
-    }
-
-    public void mostrarCuenta(String emailBuscado) {
-        for (Sucursal sucursal : sucursales) {
-            if (sucursal == null) {
-                throw new IllegalArgumentException("(Banco, mostrarCuenta) una sucursal iterada es nula");
-            }
-            Cuenta cuentaBuscada = sucursal.buscarCuenta(emailBuscado);
-            if (cuentaBuscada != null) {
-                System.out.println(cuentaBuscada);
-            }
-        }
+        sucursales.add(new Sucursal(nombre));
     }
 
     public void mostrarCuentas() {
         System.out.println("-----Detalles de las cuentas del banco-----");
         for (Sucursal sucursal : sucursales) {
-            if (sucursal == null) {
-                throw new IllegalArgumentException("(Banco, mostrarCuentas) una sucursal iterada es nula");
-            }
             System.out.println("-----Detalles de las cuentas de la sucursal " + sucursal.getNombre() + "-----");
             sucursal.mostrarCuentas();
         }
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public static Banco getInstancia(String nombre) {
+    public static Banco getInstancia(){
         if (instancia == null) {
-            instancia = new Banco(nombre);
+            instancia = new Banco();
         }
         return instancia;
-    }
-
-    private void setNombre(String nombre) {
-        if (nombre.isBlank()) {
-            throw new IllegalArgumentException("(Banco, setNombre) el nombre del banco está vacío");
-        }
-        this.nombre = nombre;
     }
 }
