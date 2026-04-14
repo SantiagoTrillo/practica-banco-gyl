8b0b163c2738c746cda3d4914c80659d4b3a14e8
Santiago Trillo <santitrillo07@gmail.com>
2026-04-14 13:13:14 -0300

    Feature/144/Atributo contraseña reintroducido e implementación de setters

--- src/main/java/Cuenta.java
+++ src/main/java/Cuenta.java
@@ -1,15 +1,16 @@
 public class Cuenta {
-    private final String nombre, email;
+    private String nombre, email, contraseña;
     private double saldo;
-    private final Sucursal sucursal;
-    private final TipoCuenta tipoCuenta;
+    private Sucursal sucursal;
+    private TipoCuenta tipoCuenta;
 
-    public Cuenta(String nombre, String email, TipoCuenta tipoCuenta, Sucursal sucursal) {
-        this.nombre = nombre;
-        this.email = email;
+    public Cuenta(String nombre, String email, String contraseña, Sucursal sucursal, TipoCuenta tipoCuenta) {
+        setNombre(nombre);
+        setEmail(email);
+        setContraseña(contraseña);
+        setSucursal(sucursal);
+        setTipoCuenta(tipoCuenta);
         saldo = 0;
-        this.sucursal = sucursal;
-        this.tipoCuenta = tipoCuenta;
     }
 
     @Override
@@ -17,7 +18,7 @@ public class Cuenta {
         return "Nombre: " + nombre + '\n' +
                 "Email: " + email + '\n' +
                 "Saldo: " + saldo +
-                "Sucursal: " + sucursal + '\n' +
+                "Sucursal: " + sucursal.getNombre() + '\n' +
                 "Tipo de cuenta: " + tipoCuenta + '\n';
     }
 
@@ -33,7 +34,45 @@ public class Cuenta {
         return saldo;
     }
 
+    public void setNombre(String nombre) {
+        if (nombre == null || nombre.isBlank()) {
+            throw new IllegalArgumentException("(Cuenta, setNombre) el nombre de la cuenta está vacío");
+        }
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email) {
+        if (email == null || email.isBlank()) {
+            throw new IllegalArgumentException("(Cuenta, setEmail) el email de la cuenta está vacío");
+        }
+        this.email = email;
+    }
+
+    public void setContraseña(String contraseña) {
+        if (contraseña == null || contraseña.isBlank() || contraseña.length() < 8) {
+            throw new IllegalArgumentException("(Cuenta, setContraseña) la contraseña de la cuenta está vacía o es demasiado corta");
+        }
+        this.contraseña = contraseña;
+    }
+
+    public void setSucursal(Sucursal sucursal) {
+        if (sucursal == null) {
+            throw new IllegalArgumentException("(Cuenta, setSucursal) la sucursal de la cuenta es nula");
+        }
+        this.sucursal = sucursal;
+    }
+
+    public void setTipoCuenta(TipoCuenta tipoCuenta) {
+        if (tipoCuenta == null) {
+            throw new IllegalArgumentException("(Cuenta, setTipoCuenta) el tipo de la cuenta está vacío");
+        }
+        this.tipoCuenta = tipoCuenta;
+    }
+
     public void setSaldo(double saldo) {
+        if (saldo < 1) {
+            throw new IllegalArgumentException("(Cuenta, setSaldo) el saldo de la cuenta no es un número positivo");
+        }
         this.saldo = saldo;
     }
 }
